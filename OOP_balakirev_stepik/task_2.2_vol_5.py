56c5cc4fd7d08a64420ec5d78d89bf18c32c1da1
flomasta <zatevi@mail.ru>
2022-07-01 14:43:43 +0300

    finished tree task

--- OOP_balakirev_stepik/task_2.2_vol_5.py
+++ OOP_balakirev_stepik/task_2.2_vol_5.py
@@ -27,6 +27,17 @@ class DecisionTree:
     def predict(cls, root, x):
         obj = root
         while obj:
+            obj_next = cls.get_next(obj, x)
+            if obj_next is None:
+                break
+            obj = obj_next
+        return obj.value
+
+    @classmethod
+    def get_next(cls, obj, x):
+        if x[obj.indx] == 1:
+            return obj.left
+        return obj.right
 
     @classmethod
     def add_obj(cls, obj, node=None, left=True):
@@ -36,3 +47,17 @@ class DecisionTree:
             else:
                 node.right = obj
         return obj
+
+
+root = DecisionTree.add_obj(TreeObj(0))
+v_11 = DecisionTree.add_obj(TreeObj(1), root)
+v_12 = DecisionTree.add_obj(TreeObj(2), root, False)
+DecisionTree.add_obj(TreeObj(-1, "будет программистом"), v_11)
+DecisionTree.add_obj(TreeObj(-1, "будет кодером"), v_11, False)
+DecisionTree.add_obj(TreeObj(-1, "не все потеряно"), v_12)
+DecisionTree.add_obj(TreeObj(-1, "безнадежен"), v_12, False)
+
+# x = [1, 1, 0]
+x = [1, 1, 0]
+res = DecisionTree.predict(root, [1, 1, 0])
+print(res)
