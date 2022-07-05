4424d385150236f33b89077884a35a620687f8ef
flomasta <zatevi@mail.ru>
2022-07-05 22:19:28 +0300

    Descriptor third task resolved

---
+++ OOP_balakirev_stepik/task_2.3_vol_3.py
@@ -0,0 +1,59 @@
+class StringValue:
+    def __init__(self, min=2, max=50):
+        self.min_length = min
+        self.max_length = max
+
+    def __set_name__(self, owner, name):
+        self.name = '_' + name
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name)
+
+    def __set__(self, instance, value):
+        if type(value) == str and self.min_length <= len(value) <= self.max_length:
+            setattr(instance, self.name, value)
+
+
+class PriceValue:
+    def __init__(self, min=0, max=10000):
+        self.min_length = min
+        self.max_length = max
+
+    def __set_name__(self, owner, name):
+        self.name = '_' + name
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name)
+
+    def __set__(self, instance, value):
+        if type(value) in (int, float) and self.min_length <= value <= self.max_length:
+            setattr(instance, self.name, value)
+
+
+class SuperShop:
+    def __init__(self, name):
+        self.name = name
+        self.goods = []
+
+    def add_product(self, product):
+        self.goods.append(product)
+
+    def remove_product(self, product):
+        if product in self.goods:
+            self.goods.remove(product)
+
+
+class Product:
+    name = StringValue()
+    price = PriceValue()
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+shop = SuperShop("У Балакирева")
+shop.add_product(Product("Курс по Python", 0))
+shop.add_product(Product("Курс по Python ООП", 2000))
+for p in shop.goods:
+    print(f"{p.name}: {p.price}")
