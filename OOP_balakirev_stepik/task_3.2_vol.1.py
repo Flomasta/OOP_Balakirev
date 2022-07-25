ef4e2d15598f31b471475c46fe1eab7a8de62021
flomasta <zatevi@mail.ru>
2022-07-25 16:01:54 +0300

    new vol started resolved first task

---
+++ OOP_balakirev_stepik/task_3.2_vol.1.py
@@ -0,0 +1,21 @@
+import random
+
+
+class RandomPassword:
+    def __init__(self, psw_chars, min_length, max_length):
+        self.psw_chars = psw_chars
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def __call__(self, *args, **kwargs):
+        return ''.join(random.sample(self.psw_chars, random.choice(range(self.min_length, self.max_length))))
+
+
+min_length = 5
+max_length = 20
+psw_chars = "qwertyuiopasdfghjklzxcvbnm0123456789!@#$%&*"
+
+rnd = RandomPassword(psw_chars, min_length, max_length)
+
+lst_pass = [rnd() for i in range(3)]
+print(lst_pass)
